e52eea9c07438c3a0d45961259483f004f0ef1c4
Siddharth <siddharth@ubuntu.ubuntu-domain>
2014-04-12 01:24:21 +0530

    Added preliminary content in the Missions page

---
+++ missions-content.php
@@ -0,0 +1,25 @@
+<span style="font-size: 25px">
+
+	<ul>
+		<li>
+			To mobilize India's youth to participate in energy policy
+			dialogue with government.
+		</li>
+		<li>
+			Engage in Capacity Building.
+		</li>
+		<li>
+
+			Increase knowledge dissemination.
+		</li>
+		<li>
+
+			Create a low cost networking platform between academia, industry and government.
+		</li>
+		<li>
+
+			Bring forward energy leaders of tomorrow
+		</li>
+	</ul>
+
+</span>
---
+++ missions.php
@@ -0,0 +1,64 @@
+<html>
+
+<head>
+
+	<title>Youth Energy Commission - Home </title>
+
+	<?php include("main-style.php"); ?>
+
+	<?php include("analytics.php"); ?>
+	
+
+	<!-- Set the active menubar tab -->
+
+	<script>
+
+		$(document).ready(function(){
+
+			$("#menu-home").addClass("active");
+
+		});
+
+
+	</script>
+	
+	
+	
+</head>
+
+<body>
+
+	<div class="container-fluid"  style="text-align: center;">
+
+		<?php include("name-and-logo.php"); ?>
+
+	</div>
+
+	<div class="container-fluid">
+
+		<div class="col-md-2" style="background: ;">
+
+			<div id="topbar" style="text-align: center">
+
+				<?php include("menubar.php"); ?>
+
+			</div>
+		</div>
+
+		<div class="col-md-8" style="background:;">
+
+			<?php include("missions-content.php"); ?>
+
+		</div>
+
+		<div class="col-md-2" style="background: ;"> 
+
+			<?php include("social-buttons.php"); ?>
+
+		</div>
+
+	</div>
+
+</body>
+
+</html>
