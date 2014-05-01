0cb406e30776459e73a6b4593dda7f10e17f7146
icyflame <kannan.siddharth12@gmail.com>
2014-05-01 15:43:05 +0530

    Completely revamped the contacts page. Now the icons are circular, names are properly sorted into according to the positions held. Font used: PTSans

--- contactText.php
+++ contactText.php
@@ -1,12 +1,16 @@
 <style>
 
-.col-md-6 a{
+.col-md-6 a, .col-md-4 a{
 	text-decoration: none !important;
 	opacity: 0.9;
 }
 
-.col-md-6 a:hover{
+.col-md-6 a:hover, .col-md-4 a:hover{
 	opacity: 1;
+}
+
+div > span:first-child{
+	font-size: 20px;
 }
 
 @font-face {
@@ -21,140 +25,50 @@
 
 <div class="container-fluid" style="font-family:PTSans; text-align: center;">
 
-	<div class="col-md-6">
+	<!-- Advisors -->
 
-		<div>
+	<div class="row" id="advisors">
+
+		<h1>Advisors</h1>
+
+	</div>
+
+	<div class="row">
+
+		<div class="col-md-6">
 			<span> Himanshu Gupta &nbsp 
+			</span><br/>
+			<span>Founder
+			</span><br/>
+			<span>
+
 				<a href="https://www.facebook.com/himanshu.gupta.121?fref=ts">
 					<span class="fa-stack fa-lg">
 						<i class="fa fa-circle fa-stack-2x" style="color: 3b5998"></i>
 						<i class="fa fa-facebook fa-stack-1x" style="color: ffffff"></i>
 					</span>
 				</a>
-			</span>
-			<span><!--Email ID-->
-			</span>
-			<span><!--Mobile Number--> 
-			</span>
-		</div><br><br>
-		<div>
-			<span> Ssd Jagannadh &nbsp
-				<a href="https://www.facebook.com/ssd.jagannadh?fref=ts">
+
+				<a href="in.linkedin.com/pub/himanshu-gupta/17/77a/834">
 					<span class="fa-stack fa-lg">
 						<i class="fa fa-circle fa-stack-2x" style="color: 3b5998"></i>
-						<i class="fa fa-facebook fa-stack-1x" style="color: ffffff"></i>
+						<i class="fa fa-linkedin fa-stack-1x" style="color: ffffff"></i>
 					</span>
 				</a>
-			</span>
-			<br>
-			<span>jagan.sripada007@gmail.com
-			</span>
-			<br>
-			<span>+919679771992 
-			</span>
-		</div> 
-
-		<br/><br/>
 
 
-		<div>
-			<span>Arijit Chatterjee &nbsp 
-				<a href="https://www.facebook.com/arijit2301?fref=ts">
-					<span class="fa-stack fa-lg">
-						<i class="fa fa-circle fa-stack-2x" style="color: 3b5998"></i>
-						<i class="fa fa-facebook fa-stack-1x" style="color: ffffff"></i>
-					</span>
-				</a>
-			</span>
-			<br>
-			<span>arijit2301@gmail.com
-			</span>
-			<br>
-			<span>+917797439016
 			</span>
 		</div>
-		<br>
-		<br>
-		<div>
-			<span>Sowmya Manukonda &nbsp 
-				<a href="https://www.facebook.com/sowmyamanu?fref=ts">
-					<span class="fa-stack fa-lg">
-						<i class="fa fa-circle fa-stack-2x" style="color: 3b5998"></i>
-						<i class="fa fa-facebook fa-stack-1x" style="color: ffffff"></i>
-					</span>
-				</a>
-			</span>
-			<br>
-			<span>sowmyamanukonda51@gmail.com
-			</span>
-			<br>
-			<span>+917501378788
-			</span>
-		</div>
-		<br>
-		<br>
-		<div>
-			<span> Akash Gupta &nbsp
-				<a href="https://www.facebook.com/TheKingofallTrolls?fref=ts">
-					<span class="fa-stack fa-lg">
-						<i class="fa fa-circle fa-stack-2x" style="color: 3b5998"></i>
-						<i class="fa fa-facebook fa-stack-1x" style="color: ffffff"></i>
-					</span>
-				</a>
-			</span>
-			<br>
-			<span>guptaakash1412@gmail.com
-			</span>
-			<br>
-			<span>+917699499369
-			</span>
-		</div>
-		<br>
-		<br>
-		<div>
-			<span> Siddharth Kannan &nbsp
-				<a href="https://www.facebook.com/kannan.siddharth">
-					<span class="fa-stack fa-lg">
-						<i class="fa fa-circle fa-stack-2x" style="color: 3b5998"></i>
-						<i class="fa fa-facebook fa-stack-1x" style="color: ffffff"></i>
-					</span>
-				</a>
-			</span>
-			<br>
-			<span>kannan.siddharth12@gmail.com
-			</span>
-			<br>
-			<span>+919800190996
-			</span>
-		</div>
-		<br>
-		<br>
-		<div>
-			<span> Asish Kumar Mishra &nbsp 
-				<a href="https://www.facebook.com/kumarasish1995?fref=ts">
-					<span class="fa-stack fa-lg">
-						<i class="fa fa-circle fa-stack-2x" style="color: 3b5998"></i>
-						<i class="fa fa-facebook fa-stack-1x" style="color: ffffff"></i>
-					</span>
-				</a>
-			</span>
-			<br>
-			<span>asishkumarmishra1000@gmail.com
-			</span>
-			<br>
-			<span>+918513902019
-			</span>
-		</div> 
 
-
-	</div>
-
-
-	<div class="col-md-6">
-
-
-		<div>
+		<div class="col-md-6">
 			<span>Rahul Agarwal &nbsp 
+
+			</span>
+			<br>
+			<span>Founder
+			</span>
+			<br>
+			<span>
 				<a href="https://www.facebook.com/r.agrawal90">
 					<span class="fa-stack fa-lg">
 						<i class="fa fa-circle fa-stack-2x" style="color: 3b5998"></i>
@@ -162,56 +76,188 @@
 					</span>
 				</a>
 			</span>
-			<br>
-			<span><!--Email ID-->
-			</span>
-			<span><!--Mobile number-->
-			</span>
 
 		</div>
-		<br/>
-		<br/>
-		<div>
-			<span> Anand Hariharan		 &nbsp
+
+	</div>
+	<br><br>
+
+	<div class="row">
+
+		<div class="col-md-6">
+			<span> Ssd Jagannadh &nbsp
+
+			</span>
+			<br>
+			<span>jagan.sripada007@gmail.com
+			</span>
+			<br>
+			<span>+919679771992 
+			</span>
+			<br>
+			<span>
+				<a href="https://www.facebook.com/ssd.jagannadh?fref=ts">
+					<span class="fa-stack fa-lg">
+						<i class="fa fa-circle fa-stack-2x" style="color: 3b5998"></i>
+						<i class="fa fa-facebook fa-stack-1x" style="color: ffffff"></i>
+					</span>
+				</a>
+			</span>
+		</div>
+		<div class="col-md-6">
+			<span>Sowmya Manukonda &nbsp 
+
+			</span>
+			<br>
+			<span>sowmyamanukonda51@gmail.com
+			</span>
+			<br>
+			<span>+917501378788
+			</span>
+			<br>
+			<span>
+				<a href="https://www.facebook.com/sowmyamanu?fref=ts">
+					<span class="fa-stack fa-lg">
+						<i class="fa fa-circle fa-stack-2x" style="color: 3b5998"></i>
+						<i class="fa fa-facebook fa-stack-1x" style="color: ffffff"></i>
+					</span>
+				</a>
+			</span>
+		</div>
+	</div>
+
+	<!-- Heads -->
+
+	<div class="row" id="heads">
+
+		<h1>Heads</h1>
+
+	</div>
+
+	<div class="row">
+
+		<div class="col-md-4">
+
+			<span> Anand Hariharan
+			</span>
+			<br>
+			<span>Head of Operations, YEC IIT KGP
+			</span>
+			<br>
+			<span>khanand.iitkgp@gmail.com
+			</span>
+			<br>
+			<span>+917699499474
+			</span>
+			<br>
+			<span>
+
 				<a href="https://www.facebook.com/anand.hariharan.96?fref=ts">
 					<span class="fa-stack fa-lg">
 						<i class="fa fa-circle fa-stack-2x" style="color: 3b5998"></i>
 						<i class="fa fa-facebook fa-stack-1x" style="color: ffffff"></i>
 					</span>
 				</a>
+
+			</span>
+
+		</div>
+
+		<div class="col-md-4">
+
+
+			<span>Arijit Chatterjee &nbsp
 			</span>
 			<br>
-			<span>khanand.iitkgp@gmail.com</span>
+			<span>Head, Financial Operations
+			</span>
 			<br>
-			<span>+917699499474 </span>
-		</div>
-		<br>
-		<br>
+			<span>arijit2301@gmail.com
+			</span>
+			<br>
+			<span>+917797439016
+			</span>
+			<br>
+			<span>
+				<a href="https://www.facebook.com/arijit2301?fref=ts">
+					<span class="fa-stack fa-lg">
+						<i class="fa fa-circle fa-stack-2x" style="color: 3b5998"></i>
+						<i class="fa fa-facebook fa-stack-1x" style="color: ffffff"></i>
+					</span>
+				</a>
 
-		<div>
-			<span> Praveen Kumar &nbsp
+			</span>
+		</div>
+
+		<div class="col-md-4">
+			<span> Praveen Kumar
+			</span>
+			<br>
+			<span>Head, Financial Operations
+			</span>
+			<br>
+			<span>praveenbbsr72@gmail.com
+			</span>
+			<br>
+			<span>+919800170230 
+			</span>
+			<br>
+			<span>
+
 				<a href="https://www.facebook.com/profile.php?id=100001483399064&fref=ts">
 					<span class="fa-stack fa-lg">
 						<i class="fa fa-circle fa-stack-2x" style="color: 3b5998"></i>
 						<i class="fa fa-facebook fa-stack-1x" style="color: ffffff"></i>
 					</span>
 				</a>
+
+			</span>
+
+		</div>
+
+	</div>
+
+	<!-- Coordinators -->
+
+	<div class="row" id="heads">
+
+		<h1>Coordinators</h1>
+
+	</div>
+
+	<div class="row">
+
+		<div class="col-md-4">
+			<span> Akash Gupta
 			</span>
 			<br>
-			<span>praveenbbsr72@gmail.com</span>
+			<span>Social Media Management
+			</span>
 			<br>
-			<span>+919800170230 </span>
-		</div>
-		<br>
-		<br>
-		<div>
-			<span>Vinay Dutta	&nbsp 
-				<a href="https://www.facebook.com/vinay.d.vemula">
+			<span>guptaakash1412@gmail.com
+			</span>
+			<br>
+			<span>+917699499369
+			</span>
+			<br>
+			<span>
+
+				<a href="https://www.facebook.com/TheKingofallTrolls?fref=ts">
 					<span class="fa-stack fa-lg">
 						<i class="fa fa-circle fa-stack-2x" style="color: 3b5998"></i>
 						<i class="fa fa-facebook fa-stack-1x" style="color: ffffff"></i>
 					</span>
 				</a>
+
+			</span>
+		</div>
+
+
+		<div class="col-md-4">
+			<span>Vinay Dutta
+			</span>
+			<br>
+			<span>Design
 			</span>
 			<br>
 			<span>vinaydutta94@gmail.com
@@ -219,35 +265,64 @@
 			<br>
 			<span>+917699476956
 			</span>
-		</div>
-		<br>
-		<br>
-		<div>
-			<span>Punit Pal &nbsp 
-				<a href="https://www.facebook.com/punit.k.pal?fref=ts">
+			<br>
+			<span>
+
+				<a href="https://www.facebook.com/vinay.d.vemula">
 					<span class="fa-stack fa-lg">
 						<i class="fa fa-circle fa-stack-2x" style="color: 3b5998"></i>
 						<i class="fa fa-facebook fa-stack-1x" style="color: ffffff"></i>
 					</span>
 				</a>
+
 			</span>
-			<br>
-			<span>punitkpal@gmail.com
-			</span>
-			<br>
-			<span>+919800125211
-			</span>
+
 		</div>
-		<br>
-		<br>
-		<div>
-			<span> Karan Gupta &nbsp
-				<a href="http://goo.gl/SwvxIm">
+
+		<div class="col-md-4">
+			<span>Vaibhav Gavhale
+			</span>
+			<br>
+			<span>Web
+			</span>
+			<br>
+			<span>vaibhav.gavhale@facebook.com
+			</span>
+			<br>
+			<span>+917699577484
+			</span>
+			<br>
+			<span>
+
+				<a href="https://www.facebook.com/vaibhav.gavhale">
 					<span class="fa-stack fa-lg">
 						<i class="fa fa-circle fa-stack-2x" style="color: 3b5998"></i>
 						<i class="fa fa-facebook fa-stack-1x" style="color: ffffff"></i>
 					</span>
 				</a>
+
+			</span>
+
+		</div>
+
+	</div>
+
+	<!-- Associate Managers -->
+
+	<div class="row" id="heads">
+
+		<h1>Associate Managers</h1>
+
+	</div>
+
+	<div class="row">
+
+		<div class="col-md-4">
+
+			<span> Karan Gupta
+			</span>
+			<br>
+			<span>Events and Corporate Relations
 			</span>
 			<br>
 			<span>karangupta21893@gmail.com
@@ -255,17 +330,106 @@
 			<br>
 			<span>+918609267909
 			</span>
-		</div>
-		<br>
-		<br>
-		<div>
-			<span> Jagannadh Sripada &nbsp 
-				<a href="https://www.facebook.com/jagannadh.sripada?fref=ts">
+			<br>
+			<span>
+
+				<a href="http://goo.gl/SwvxIm">
 					<span class="fa-stack fa-lg">
 						<i class="fa fa-circle fa-stack-2x" style="color: 3b5998"></i>
 						<i class="fa fa-facebook fa-stack-1x" style="color: ffffff"></i>
 					</span>
 				</a>
+
+			</span>
+
+		</div>
+
+		<div class="col-md-4">
+
+			<span>Punit Pal
+			</span>
+			<br>
+			<span>Corporate Relations and Events
+			</span>
+			<br>
+			<span>punitkpal@gmail.com
+			</span>
+			<br>
+			<span>+919800125211
+			</span>
+			<br>
+			<span>
+				<a href="https://www.facebook.com/punit.k.pal?fref=ts">
+					<span class="fa-stack fa-lg">
+						<i class="fa fa-circle fa-stack-2x" style="color: 3b5998"></i>
+						<i class="fa fa-facebook fa-stack-1x" style="color: ffffff"></i>
+					</span>
+				</a>
+			</span>
+
+
+		</div>
+
+		<div class="col-md-4">
+
+			<span> Siddharth Kannan
+			</span>
+			<br>
+			<span>Web and Content Development
+			</span>		
+			<br>
+			<span>kannan.siddharth12@gmail.com
+			</span>
+			<br>
+			<span>+919800190996
+			</span>
+			<br>
+			<span>
+				<a href="https://www.facebook.com/kannan.siddharth">
+					<span class="fa-stack fa-lg">
+						<i class="fa fa-circle fa-stack-2x" style="color: 3b5998"></i>
+						<i class="fa fa-facebook fa-stack-1x" style="color: ffffff"></i>
+					</span>
+				</a>
+			</span>
+
+		</div>
+
+	</div>
+	<br><br>
+
+	<div class="row">
+
+		<div class="col-md-6">
+			<span> Asish Kumar Mishra
+			</span>
+			<br>
+			<span>Web
+			</span>
+			<br>
+			<span>asishkumarmishra1000@gmail.com
+			</span>
+			<br>
+			<span>+918513902019
+			</span>
+			<br>
+			<span>
+				<a href="https://www.facebook.com/kumarasish1995?fref=ts">
+					<span class="fa-stack fa-lg">
+						<i class="fa fa-circle fa-stack-2x" style="color: 3b5998"></i>
+						<i class="fa fa-facebook fa-stack-1x" style="color: ffffff"></i>
+					</span>
+				</a>
+			</span>
+		</div>
+
+
+		<div class="col-md-6">
+
+			<span> Jagannadh Sripada							
+			</span>
+			<br>
+			<span>Finance and Logistics
 			</span>
 			<br>
 			<span>jagannadh786@gmail.com
@@ -273,9 +437,17 @@
 			<br>
 			<span>+919800109522
 			</span>
-		</div> 
+			<br>
+			<span>
+				<a href="https://www.facebook.com/jagannadh.sripada?fref=ts">
+					<span class="fa-stack fa-lg">
+						<i class="fa fa-circle fa-stack-2x" style="color: 3b5998"></i>
+						<i class="fa fa-facebook fa-stack-1x" style="color: ffffff"></i>
+					</span>
+				</a>
+			</span>
 
+		</div>
 
 	</div>
-
 </div>
