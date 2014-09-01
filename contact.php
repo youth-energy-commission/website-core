bff55bb6b82c0bc971e8786b09289660fed00278
icyflame <kannan.siddharth12@gmail.com>
2014-09-01 13:24:13 +0530

    Revamped contacts page. Moved the advisors to the bottom of the contacts page. (Font set to the default font-awesome font)

---
+++ contact.php
@@ -0,0 +1,93 @@
+<html>
+
+<head>
+
+   <title>Youth Energy Commission - Home </title>
+
+   <?php include("mainInclude.php"); ?>
+
+   <!-- Set the active menubar tab -->
+
+   <script>
+
+   $(document).ready(function(){
+
+      $("#contactus").addClass("active");
+
+   });
+
+   </script>
+   
+</head>
+
+<body>
+
+   <div class="container-fluid" >
+
+      <div class="col-md-2" style="background: ;">
+
+         <a href="http://yecindia.com" style="text-decoration: none;">
+            <img src="logo.jpg" width="150" height="150"/><br/>
+         </a>
+
+      </div>
+
+      <div class="col-md-8" style="background:; text-align: center; ">
+
+         <span style="font-size: 40px; line-height:3em">Youth Energy Commission </span>
+
+         <?php include("menubar.php"); ?>
+
+      </div>
+
+      <div class="col-md-2" style="background: ; text-align: center; ">
+
+         <?php include("socialIcons.php"); ?>
+
+      </div>
+
+   </div>
+
+   <hr/>
+
+   <div class="container-fluid">
+
+      <div class="col-md-2" >
+
+
+      </div>
+
+
+      <div class="col-md-8" style="background:;">
+
+         <?php include("contactText.php"); ?>
+
+
+      </div>
+
+      <div class="col-md-2" style="background: ;">
+         
+         <div style="text-align: center; font-color: black;">
+
+            <br/><br/>
+
+            <a href="http://goo.gl/aUBHVx" target="_blank" class="connect-button"><button class="btn btn-info">Have an idea? <br/>Share it with us.</button>
+            </a>
+            <br/><br/>
+
+         </div>
+         <br/>
+
+         <a class="twitter-timeline" href="https://twitter.com/YEC_India" data-widget-id="457555811586154496">Tweets by @YEC_India</a>
+         <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+      </div>
+
+   </div>
+
+</div>
+
+</div>
+
+</body>
+
+</html>
