e7c31cdacfe8cc30c5aa68d2d256f8495cc1a707
icyflame <kannan.siddharth12@gmail.com>
2014-09-01 16:01:43 +0530

    Isolated the code for the associate button and the twitter feed.

---
+++ assocButtons.php
@@ -0,0 +1,12 @@
+<div style="text-align: center; font-color: black;">
+
+	<br/><br/>
+
+	<a href="http://goo.gl/aUBHVx" target="_blank" class="connect-button">
+		<button class="btn btn-info">
+			Have an idea? <br/>Share it with us.
+		</button>
+	</a>
+	<br/><br/>
+
+</div>
