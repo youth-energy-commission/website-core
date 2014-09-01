65e7d1b2276d6463eeea1274ae1ece1889997cba
icyflame <kannan.siddharth12@gmail.com>
2014-09-01 13:25:24 +0530

    The noticeboard to be included in the left pane of all the pages.

---
+++ noticeboard.php
@@ -0,0 +1,36 @@
+<div style="background-color: black; opacity: 0.5; color: white; border-radius: 10px; ">
+
+	<br/>
+
+	<div style="margin-left: 20px; margin-right: 20px; margin-top: 10px;">
+
+		<span style="text-align: center; ">
+
+			<h3> Latest News </h3>
+
+		</span>
+
+		<hr/>
+
+		<h4>
+
+			The first issue of our newsletter is coming soon.
+
+		</h4>
+
+		<hr/>
+
+		<br/>
+		<br/>
+		<br/>
+
+		<hr/>
+
+		<h4>
+			Check this space for all the latest news.
+		</h4>
+
+		<br/>
+
+	</div>
+</div>
