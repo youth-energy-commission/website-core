bfc8366619beece1aca4bb513e3cfb3cbbc47b92
icyflame <kannan.siddharth12@gmail.com>
2014-09-01 15:21:55 +0530

    Added the text in the aboutus text file from the YEC annual report.

--- aboutusText.php
+++ aboutusText.php
@@ -1,1 +1,40 @@
-YEC was founded by Himanshu Gupta.
+<span class="fa" style="font-size: 25px; ">
+
+	<b>Youth Energy Commission</b> aims to become a catalyst for accelerating the growth of youth in the energy sector. <br/>
+
+	YEC would provide a common platform for interaction between the academia, industries and the government. 
+	It will provide the youth with the opportunities to explore exciting career prospects in energy sector and to 
+	remain updated about the latest happenings of energy sector. <br/> <br/>
+
+	For the first time in India, youth will get a chance to voice their opinion directly to the policymakers 
+	and to network with professionals across the energy industry. <br/> <br/>
+
+	YEC would enable youth to showcase their innovation to the leading firms in the energy sector
+	and increase their product reach. A series of conferences, networking events, venture capital pitches, 
+	events, exhibitions, an opportunity to write articles and policy papers is what awaits the students associated with YEC. 
+
+	<hr/>
+
+	<h3>Missions<h3>
+
+	<ul>
+		<li>
+			Create a low cost networking platform between academia, industry & government.
+		</li>
+		<li>
+			To mobilize India's youth to participate in energy policy dialogue with government.
+		</li>
+		<li>
+			Engage in Capacity Building.
+		</li>
+		<li>
+			Increase knowledge dissemination.
+		</li>
+		<li>
+			Bring forward energy leaders of tomorrow.
+		</li>
+	</ul>
+
+	<hr/>
+
+</span>
