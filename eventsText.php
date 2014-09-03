e664e7690b19319ba3c493b2a5825f73d7369f39
icyflame <kannan.siddharth12@gmail.com>
2014-09-03 18:33:39 +0530

    Added the events text page.
    Used the collapsible feature of bootstrap

--- eventsText.php
+++ eventsText.php
@@ -1,1 +1,105 @@
-YEC IIT Kharagpur has conducted three guest lectures.
+<div class="panel-group" id="accordion">
+
+  <div class="panel panel-default">
+    <div class="panel-heading">
+
+      <h1>Open House Session on Energy Sector</h1>
+
+      <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" style="float: right; ">
+        <button class="btn btn-lg btn-primary" class="toggleText">
+
+          <i class="fa fa-list-ul"></i> Toggle
+        </button>
+      </a>
+
+      <h2>Raghav Trivedi and Arun Chidambaram</h2>
+      <h3>Our founder, Himanshu Gupta, also participated in this discussion.</h3>
+      <h3>28th March, 2014</h3>
+
+      
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse">
+      <div class="panel-body">
+       <h3>
+
+        Mr. Raghav Trivedi is the COO of Moserbaer Energy, and Mr. Arun Chidambaram, is a Geo-data specialist at Conoco Philips.
+        <br/><br/>
+        With a combined experienced of almost 50 years, both of them, gave the layman a view into the complicated Energy sector,
+        the dynamics of energy usage, and taking help of statistics in the public domain, and putting them together, Mr. Trivedi,
+        made everyone believe in the complex workings of the Energy Industry, vested interests, and the traction that startups gain.
+        <br/><br/>
+        Mr. Chidambaram, gave us a deeper view into the types of machinery that is used in the mining of data, from industries and power plants
+        both. He explained the complexity and the ingenuity, with which these instruments have been designed.
+
+      </h3>
+    </div>
+  </div>
+</div>
+
+<div class="panel panel-default">
+  <div class="panel-heading">
+
+    <h1>Megalith: Guest lecture by Prabhat Kumar Nath</h1>
+
+    <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" style="float: right; ">
+      <button class="btn btn-lg btn-primary" class="toggleText">
+
+        <i class="fa fa-list-ul"></i> Toggle
+      </button>
+    </a>
+
+    <h2>Senior VP, Reliance Infrastructure</h2>
+
+    <h3>14th February, 2014</h3>
+
+  </div>
+  <div id="collapseTwo" class="panel-collapse collapse">
+    <div class="panel-body">
+
+      <h3>
+
+        This event was conducted in association with Megalith, the Civil Engg Department's fest, in IIT Kharagpur.
+
+      </h3>
+
+    </div>
+  </div>
+</div>
+
+<div class="panel panel-default">
+  <div class="panel-heading">
+
+   <h1>Networking Session with Mr. Vinay Jaju</h1>
+
+   <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree" style="float: right; ">
+    <button class="btn btn-lg btn-primary" class="toggleText">
+
+      <i class="fa fa-list-ul"></i> Toggle
+    </button>
+  </a>
+
+  <h2>COO, Onergy (Energy Startup)</h2>
+
+  <h3>4th September, 2013</h3>
+
+</div>
+<div id="collapseThree" class="panel-collapse collapse">
+  <div class="panel-body">
+    
+    <h3>
+
+      YEC enabled a lot of MBA and Law students, to meet this man, who has become one of the most successful entreprenuers, with his 
+      energy startup Onergy. 
+
+      <br/><br/>
+
+      YEC hopes that this success story inspires everyone to move towards the energy industry.
+
+    </h3>
+  </div>
+</div>
+</div>
+</div>
+
+<br/>
+<br/>
