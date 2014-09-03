8c35a273c399b7c0bf61e682ea97d561903f74ca
icyflame <kannan.siddharth12@gmail.com>
2014-09-03 18:34:01 +0530

    Removed the html, head and body tags in the mailing list file

--- mailing-list.php
+++ mailing-list.php
@@ -1,101 +1,87 @@
-<html>
+<div style="margin-left: 5px; margin-right: 5px; text-align: center; ">
 
-<head>
+	<hr/>
 
-	<script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<input id="emailidreal" type="text" name="email" class="form-control" placeholder="Enter your email ID"></input>
 
-</head>
+	<br/>
 
-<body>
+	<button id="submitData" class="btn btn-success">Add to mailing list</button>
 
-	<div style="margin-left: 5px; margin-right: 5px; text-align: center; ">
+	<hr/>
 
-		<hr/>
+	<div id="response"></div>
 
-		<input id="emailidreal" type="text" name="email" class="form-control" placeholder="Enter your email ID"></input>
+</div>
 
-		<br/>
+<div class="modal fade" id="successful" style="z-index:10003" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content" >
 
-		<button id="submitData" class="btn btn-success">Add to mailing list</button>
+			<div class="modal-header" style="text-align: center; ">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h4 class="modal-title" id="myModalLabel">Email ID successfully added.</h4>
+			</div>
 
-		<hr/>
+			<div class="modal-body" style="text-align: center; ">
 
-		<div id="response"></div>
+				<p> Your email ID has been added to the mailing list. You will start getting the newsletter from the next issue.
+				</p>
 
-	</div>
+			</div>
 
-	<div class="modal fade" id="successful" style="z-index:10003" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-		<div class="modal-dialog modal-lg">
-			<div class="modal-content" >
-
-				<div class="modal-header" style="text-align: center; ">
-					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-					<h4 class="modal-title" id="myModalLabel">Email ID successfully added.</h4>
-				</div>
-
-				<div class="modal-body" style="text-align: center; ">
-
-					<p> Your email ID has been added to the mailing list. You will start getting the newsletter from the next issue.
-					</p>
-
-				</div>
-
-				<div class="modal-footer">
-					<button type="button" class="btn btn-success" data-dismiss="modal">Close</button>
-				</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-success" data-dismiss="modal">Close</button>
 			</div>
 		</div>
 	</div>
+</div>
 
-	<div class="modal fade" id="unsuccessful" style="z-index:10003" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-		<div class="modal-dialog modal-lg">
-			<div class="modal-content" >
+<div class="modal fade" id="unsuccessful" style="z-index:10003" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content" >
 
-				<div class="modal-header" style="text-align: center; ">
-					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-					<h4 class="modal-title" id="myModalLabel">Request could not be executed</h4>
-				</div>
+			<div class="modal-header" style="text-align: center; ">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h4 class="modal-title" id="myModalLabel">Request could not be executed</h4>
+			</div>
 
-				<div class="modal-body" style="text-align: center; ">
+			<div class="modal-body" style="text-align: center; ">
 
-					<p> Something's not right. We are unable to process your request :'( Please try again after some time.
-					</p>
+				<p> Something's not right. We are unable to process your request :'( Please try again after some time.
+				</p>
 
-				</div>
+			</div>
 
-				<div class="modal-footer">
-					<button type="button" class="btn btn-success" data-dismiss="modal">Close</button>
-				</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-success" data-dismiss="modal">Close</button>
 			</div>
 		</div>
 	</div>
+</div>
 
-	<div class="modal fade" id="valunsu" style="z-index:10003" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-		<div class="modal-dialog modal-lg">
-			<div class="modal-content" >
+<div class="modal fade" id="valunsu" style="z-index:10003" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content" >
 
-				<div class="modal-header" style="text-align: center; ">
-					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-					<h4 class="modal-title" id="myModalLabel">Validation Unsuccessful</h4>
-				</div>
+			<div class="modal-header" style="text-align: center; ">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h4 class="modal-title" id="myModalLabel">Validation Unsuccessful</h4>
+			</div>
 
-				<div class="modal-body" style="text-align: center; ">
+			<div class="modal-body" style="text-align: center; ">
 
-					<p> Your email ID could not be validated. Note that email IDs must always be of the form john.doe@domain.com
-					</p>
+				<p> Your email ID could not be validated. Note that email IDs must always be of the form john.doe@domain.com
+				</p>
 
-				</div>
+			</div>
 
-				<div class="modal-footer">
-					<button type="button" class="btn btn-success" data-dismiss="modal">Close</button>
-				</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-success" data-dismiss="modal">Close</button>
 			</div>
 		</div>
 	</div>
-
-</body>
+</div>
 
 <script type="text/javascript">
 
@@ -128,5 +114,3 @@ $(document).ready(function(){
 });
 
 </script>
-
-</html>
