cdab89099a5c72c220b95454acc0a231f9e2c18a
icyflame <kannan.siddharth12@gmail.com>
2014-09-03 12:22:35 +0530

    Added the mailing list in the right column

--- assocButtons.php
+++ assocButtons.php
@@ -1,12 +1,16 @@
 <div style="text-align: center; font-color: black;">
 
-	<br/><br/>
+	<h3> Get our newsletter as soon as we release it! </h3>
+
+	<?php include("mailing-list.php"); ?>
 
 	<a href="http://goo.gl/aUBHVx" target="_blank" class="connect-button">
 		<button class="btn btn-info">
 			Have an idea? <br/>Share it with us.
 		</button>
 	</a>
-	<br/><br/>
+	<br/>
+
+
 
 </div>
